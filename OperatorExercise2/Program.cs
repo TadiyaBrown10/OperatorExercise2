de7da9c12b2020bb4427884126e5685593caeb79
Tadiya Brown <tadiyabrown2000@gmail.com>
2022-08-01 22:02:57 -0400

    initial commit

---
+++ OperatorExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperatorExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle? ");
+            var radius = double.Parse(Console.ReadLine());
+
+            var areaOfCircle = Math.PI * (radius * radius);
+
+            Console.WriteLine($"The area of a circle with {radius} is {areaOfCircle}");
+
+
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+        
+    }
+}
